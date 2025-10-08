b76a2418ba7f59bc3b1fabb38d2484a7bdfcb3f2
Muhammad Waleed <mwaleed@aximfsg.com>
2025-10-08 17:58:06 +0500

    Missing imports handled

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,6 @@
 const authController = require('./auth.controller');
+const postController = require('./post.controller');
+const commentController = require('./comment.controller');
 
 module.exports = {
   authController,
